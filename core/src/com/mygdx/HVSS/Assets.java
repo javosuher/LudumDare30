07e1cab1ce8b86a4549f1520a2240d79dd74e3a7
javosuher <javier.osunaherrera@alum.uca.es>
2014-08-24 22:46:51 +0200

    Fix Sounds problem

--- core/src/com/mygdx/HVSS/Assets.java
+++ core/src/com/mygdx/HVSS/Assets.java
@@ -5,15 +5,15 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 
 public final class Assets {
-	public final static Texture background = new Texture(Gdx.files.internal("background.png"));
-	public final static Texture ship = new Texture(Gdx.files.internal("ship.png"));
-	public final static Texture shipS = new Texture(Gdx.files.internal("shipS.png"));
-	public final static Texture samuel = new Texture(Gdx.files.internal("samuel.png"));
-	public final static Texture portal = new Texture(Gdx.files.internal("portal.png"));
+	public final static Texture background = new Texture(Gdx.files.internal("Images/background.png"));
+	public final static Texture ship = new Texture(Gdx.files.internal("Images/ship.png"));
+	public final static Texture shipS = new Texture(Gdx.files.internal("Images/shipS.png"));
+	public final static Texture samuel = new Texture(Gdx.files.internal("Images/samuel.png"));
+	public final static Texture portal = new Texture(Gdx.files.internal("Images/portal.png"));
 	
-	public final static Sound ag = Gdx.audio.newSound(Gdx.files.internal("ag.wav"));
-	public final static Sound run = Gdx.audio.newSound(Gdx.files.internal("run .wav"));
-	public final static Sound win = Gdx.audio.newSound(Gdx.files.internal("win.wav"));
-	public final static Sound lose = Gdx.audio.newSound(Gdx.files.internal("lose.wav"));
-	public final static Sound portalS = Gdx.audio.newSound(Gdx.files.internal("portal.wav"));
+	public final static Sound ag = Gdx.audio.newSound(Gdx.files.internal("Sounds/ag.wav"));
+	public final static Sound run = Gdx.audio.newSound(Gdx.files.internal("Sounds/run.wav"));
+	public final static Sound win = Gdx.audio.newSound(Gdx.files.internal("Sounds/win.wav"));
+	public final static Sound lose = Gdx.audio.newSound(Gdx.files.internal("Sounds/lose.wav"));
+	public final static Sound portalS = Gdx.audio.newSound(Gdx.files.internal("Sounds/portal.wav"));
 }
